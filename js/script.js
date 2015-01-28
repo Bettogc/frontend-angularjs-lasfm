04df1dfcaf7d2b5895cd467f11524e9f65fd1aee
Marcos Rodriguez <marcos_roag@hotmail.com>
2015-01-28 12:07:17 -0600

    Music Angular Final

--- js/script.js
+++ js/script.js
@@ -1,16 +1,14 @@
-// Javascript Code.
-angular.module('app', [])
-.controller('PasswordController', function PasswordController($scope) {
-  $scope.title = "This is AngularJS and Jasmin!";
-  $scope.password = '';
-  $scope.grade = function() {
-    var size = $scope.password.length;
-    if (size > 8) {
-      $scope.strength = 'strong';
-    } else if (size > 3) {
-      $scope.strength = 'medium';
-    } else {
-      $scope.strength = 'weak';
-    }
-  };
-});
+/*Creacion de Función que Llevará Music*/
+(function(){
+    'use strict';
+    var app=angular.module("appMusic",[]);
+    /* Imagenes del JSON*/
+	app.filter("soloUrl", function(){
+	return function(item){
+		return (JSON.stringify(item)).slice(10,-18);
+		};
+	});
+    app.controller("musicController",function($scope, $http){
+        $http.get("http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=06dcac510f13bd9f9e4beb4c7b0bdb60&format=json").success(function(response) {$scope.musics = response.tracks.track;});
+    });
+})();
